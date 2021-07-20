11ceda91f409d9ae254d6458e34a01c856983118
Ilya Kharlamov <ilia3546@me.com>
2021-07-20 20:50:07 +0300

    Update Project.swift

--- Project.swift
+++ Project.swift
@@ -1,11 +1,21 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let project = Library(
+let project = Project(
     name: .Fastis,
-    options: .includeResources,
-    dependencies: TargetDependencies(
-        thirdParty: [.JTAppleCalendar, .SnapKit, .PrettyCards]
-    ),
+    targets: [
+        Target(
+            name: .Fastis,
+            resources: .defaultResourcesPath,
+            dependencies: [
+                .carthage(.JTAppleCalendar),
+                .carthage(.SnapKit),
+                .carthage(.PrettyCards)
+            ]
+        )
+    ],
+    schemes: [
+        Scheme(name: .Fastis)
+    ],
     additionalFiles: ["README.MD", "Package.swift", "Fastis.podspec"]
-).project
+)
