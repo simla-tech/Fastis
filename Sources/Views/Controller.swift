de041f6dbab2f392c4fbeea77d1f870635be2428
Ilya Kharlamov <ilia3546@me.com>
2023-09-13 16:04:30 +0300

    Fix Xcode 15 build error

--- Sources/Views/Controller.swift
+++ Sources/Views/Controller.swift
@@ -214,7 +214,11 @@ open class FastisController<Value: FastisValue>: UIViewController, JTACMonthView
      The block to execute after "Done" button will be tapped
      */
     @available(*, unavailable, message: "use dismissHandler: ((DismissAction) -> Void)?")
-    public var doneHandler: ((Value?) -> Void)?
+    public var doneHandler: ((Value?) -> Void)? {
+        get { nil }
+        // swiftlint:disable:next unused_setter_value
+        set { }
+    }
 
     /**
      And initial value which will be selected by default
